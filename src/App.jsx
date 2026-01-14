e72f248abb4cf42b3a6fa9c6f011476b985e503d
Allan Ratemo <allanisratemo@gmail.com>
2026-01-14 13:51:19 +0300

    update component import paths to use components subdirectory

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import Header from "./Header";
-import Hero from "./Hero";
-import About from "./About";
-import Services from "./Services";
-import Upcoming from "./Upcoming";
-import Footer from "./Footer";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Services from "./components/Services";
+import Upcoming from "./components/Upcoming";
+import Footer from "./components/Footer";
 
 function App() {
   return (
